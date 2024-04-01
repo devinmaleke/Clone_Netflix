9b41dee65c175da2f2db704143a3979c5364468d
Devin <devin@cyberindo-sinergi.com>
2024-04-01 13:46:54 +0700

    Initial Commit

---
+++ NetflixClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetflixClone
+//
+//  Created by Cyberindo Sinergi Sistem on 01/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
